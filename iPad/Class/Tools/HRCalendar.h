19b75ffaf8fd5ccc84842cbdd28d47270888a8f0
li <l1649675100@163.com>
2017-03-28 15:42:00 +0800

    add calendar

---
+++ iPad/Class/Tools/HRCalendar.h
@@ -0,0 +1,15 @@
+//
+//  HRCalendar.h
+//  iPad
+//
+//  Created by LI on 2017/3/28.
+//  Copyright © 2017年 LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRCalendar : UIView
+
+
+
+@end
---
+++ iPad/Class/Tools/HRCalendar.m
@@ -0,0 +1,21 @@
+//
+//  HRCalendar.m
+//  iPad
+//
+//  Created by LI on 2017/3/28.
+//  Copyright © 2017年 LI. All rights reserved.
+//
+
+#import "HRCalendar.h"
+
+@implementation HRCalendar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
